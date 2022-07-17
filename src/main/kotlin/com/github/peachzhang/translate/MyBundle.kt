5ef07cc6791c24de31c7a290c9ca0bc9ad4e4982
GitHub Action <action@github.com>
2022-07-17 01:45:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/peachzhang/translate/MyBundle.kt
+++ src/main/kotlin/com/github/peachzhang/translate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.peachzhang.translate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/peachzhang/translate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/peachzhang/translate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.peachzhang.translate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.peachzhang.translate.MyBundle
 
 class MyApplicationService {
 
